7981466449855bd5549cc33bdae27f8d66a18bdc
sfaning <82441031+sfaning@users.noreply.github.com>
2021-09-23 19:37:10 +0800

    Add files via upload

---
+++ Lab2_test.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+int main(){
+    int i=1;
+    // int int int int int int int int int int 
+    double j=0;
+    long f;
+    switch(i){
+        case 0:
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        default:
+            break;
+    }
+    if(mp3[(x-1)*n+y] && (x != p || y != q))return;
+	if(mp1[(x-2)*n+y] == 'D')
+	{
+		totdj += mp2[(x-2)*n+y];
+		dfs(x-1,y);
+		mp3[(x-2)*n+y] = true;
+	}
+	if(){
+		if(){
+		
+		}
+		else{}
+	}
+    switch(i){
+        case 0:
+            break;
+        case 1:
+            break;
+        default:
+            break;
+    }
+    if(i<0){
+        if(i<-1){}
+        else{}
+    }
+    else if(i>0){
+        if (i>2)
+        else if (i==2) {}
+        else if (i>1) {}
+        else {}
+    }
+    else{
+        if(j!=0){}
+        else{}
+    }
+    return 0;
+}
